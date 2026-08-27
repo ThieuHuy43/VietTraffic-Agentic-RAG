c1fb114fad5d744a08e44dc58b4cc8887cf0f883
ThieuHuy43 <n.thieuhuy43@gmail.com>
2026-08-28 03:48:09 +0700

    Remove unused scratch/debug files
    Leftover Tavily/HITL debug scripts from earlier development, not part of the app.

--- scratch2.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
-import inspect
-
-print("Methods in wrapper:")
-print([m for m in dir(TavilySearchAPIWrapper) if not m.startswith("_")])
